783a78fcbe0ee98f1616f38aedb83fe3f2b8fc5a
ТЕХНОРАЙ <@DESKTOP-E2H7NN2>
2024-10-06 19:25:21 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+
+                //Task1
+                {
+                    if (args is null)
+                    {
+                        throw new ArgumentNullException(nameof(args));
+                    }
+
+                    Console.Write("Введіть двозначне число N: ");
+                    int N = int.Parse(Console.ReadLine());
+
+                    int i = 1; 
+                    int sum = 0;
+
+                    Console.WriteLine("Непарні числа від 1 до {0}:", N);
+
+               
+                    while (i <= N)
+                    {
+                        if (i % 2 != 0)
+                        {
+                            Console.WriteLine(i); 
+                            sum += i; 
+                        }
+                        i++; 
+                    }
+
+                    
+                    Console.WriteLine("Сума непарних чисел: " + sum);
+                }
+            }
+        }
+    }
+}    
